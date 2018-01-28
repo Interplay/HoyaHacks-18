167686dff0bb3122cbc6a50769cff7d7c1bd56e9
Interplay <machinate@live.com>
2018-01-28 06:50:48 -0500

    more fix ups

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,21 @@
 $( window ).on( "load", function() {
     $('.dropdown').dropdown('open');
+
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('./service-worker.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+    initialiseUI();
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+  } else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
+  }
 });
